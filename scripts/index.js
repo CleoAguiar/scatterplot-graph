e4ca97d322f99330169c23f8827d7c57567f4a23
Cleo Aguiar <cleotaisas@gmail.com>
2019-07-30 21:45:57 -0300

    feat: add title and axis. User Story #1, #2 and #3

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
-    console.log('first script');
+    var url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
+    var margin = {top: 100, bottom: 30, left: 60, right: 20},
+        width = 920 - margin.left - margin.right,
+        height = 630 - margin.top - margin.bottom;
+
+    var svg = d3.select('.scatterplotGraph')
+                .append('svg')
+                .attr('width', width + margin.left + margin.right)
+                .attr('height', height + margin.top + margin.bottom);
+
+    d3.json(url).then(function(data) {
+        // if (error) throw error;
+
+        data.forEach(element => {
+            element.Place = +element.Place;
+            var parsedTime = element.Time.split(':');
+            element.Time = new Date(1970, 0, 1, 0, parsedTime[0], parsedTime[1]);
+        });
+
+        // Title
+        svg.append('text')
+            .attr('id', 'title')
+            .attr('x', width/2)
+            .attr('y', margin.top / 2)
+            .text('Doping in Professional Bicycle Racing');
+
+        svg.append('g')
+            .attr('class', 'x axis')
+            .attr('id', 'x-axis');
+
+        svg.append('g')
+            .attr('class', 'y axis')
+            .attr('id', 'y-axis');
+
+    });
 });
